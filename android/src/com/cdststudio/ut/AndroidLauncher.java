497eabd9889c2cc22e90e717c2e166367fede154
DaeSeok_Song <zxdsaqwe1231@naver.com>
2021-08-22 13:04:05 +0900

    docs: Init project

---
+++ android/src/com/cdststudio/ut/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cdststudio.ut;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cdststudio.ut.UniversityTycoon;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UniversityTycoon(), config);
+	}
+}
