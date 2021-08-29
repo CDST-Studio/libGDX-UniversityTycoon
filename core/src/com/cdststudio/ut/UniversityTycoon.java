2cad3605d0d9d5e598ed7b147ad1a069c710f965
DaeSeok_Song <zxdsaqwe1231@naver.com>
2021-08-29 14:02:05 +0900

    docs: Character movement coding in progress and libGDX's "tools" library is added

--- core/src/com/cdststudio/ut/UniversityTycoon.java
+++ core/src/com/cdststudio/ut/UniversityTycoon.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.cdststudio.ut.ViewModel.InputProcessor.ViewportInputProcessor;
@@ -17,6 +20,10 @@ public class UniversityTycoon extends ApplicationAdapter{
 	private OrthographicCamera camera;
 	private ViewportInputProcessor vip;
 	private ExtendViewport viewport;
+
+	private TextureAtlas textureAtlas;
+	private Animation<TextureRegion> animation;
+	private float elapsedTime = 0f;
 
 	Texture img;
 	float viewportWidth, viewportHeight;
@@ -39,16 +46,20 @@ public class UniversityTycoon extends ApplicationAdapter{
 		sprite = new Sprite(img, (int)0, (int)0, (int)Size_Width, (int)Size_Height); // 실제 이미지 Sprite할 크기 설정
 		sprite.setPosition(viewportWidth/2-Size_Width/2, viewportHeight/2-Size_Height/2); // 이미지 위치 값 중심에 맞추기
 
+		// 캐릭터 설정
+		textureAtlas = new TextureAtlas(Gdx.files.internal("rawSprites/pack.atlas"));
+		animation = new com.badlogic.gdx.graphics.g2d.Animation<TextureRegion>(0.025f, textureAtlas.getRegions());
+
 		// 배경 설정
 		backgroundTexture = new Texture("test.jpg");
-		backgroundSprite =new Sprite(backgroundTexture);
+		backgroundSprite = new Sprite(backgroundTexture);
 
 		camera = new OrthographicCamera(); // 카메라 초기화 (2D 게임에서 주로 쓰는 카메라 OrthographicCamera)
 
 		//Viewport(사용자에게 보여지는 영역) 크기 설정 (시점 이동까지 설정된 것은 X)
 		viewport = new ExtendViewport(1000,1000, camera);
 		viewport.apply();
-		camera.zoom = 1.0F; // 카메라 줌 설정 (1배 Ex. 2.0F == 1/2배, 0.5F == 2배 줌)
+		camera.zoom = 0.4F; // 카메라 줌 설정 (1배 Ex. 2.0F == 1/2배, 0.5F == 2배 줌) / 기본 0.4, 최대 축소 1
 
 		// InputProcessor 설정
 		vip = new ViewportInputProcessor(camera, viewportWidth, viewportHeight); // 카메라(viewport)를 위한 InputProcessor 초기화
@@ -58,6 +69,8 @@ public class UniversityTycoon extends ApplicationAdapter{
 	@Override
 	public void render () {
 		// create에서 초기화 했던걸 그려주는 단계, 프로그램 종료시까지 반복 실행
+		elapsedTime += Gdx.graphics.getDeltaTime();
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		camera.update(); // camera 정보 업데이트
@@ -67,6 +80,7 @@ public class UniversityTycoon extends ApplicationAdapter{
 		mainBatch.setProjectionMatrix(camera.combined); // 카메라 시점을 해당 객체에 고정
 		mainBatch.begin(); // .begin == 해당 객체 그리기 시작
 		backgroundSprite.draw(mainBatch); // 배경 그리기
+		mainBatch.draw(animation.getKeyFrame(elapsedTime, true), 400, 400);
 		mainBatch.draw(img, 0, 0); // 객체의 이미지 및 (x, y)위치 설정
 		mainBatch.end(); // 객체 그리기 끝
 	}
@@ -82,6 +96,7 @@ public class UniversityTycoon extends ApplicationAdapter{
 	public void dispose () {
 		// 객체들의 메모리 해제 단계
 		mainBatch.dispose(); // .dispose == 해당 객체의 메모리 해제
+		textureAtlas.dispose();
 		img.dispose();
 	}
 }
